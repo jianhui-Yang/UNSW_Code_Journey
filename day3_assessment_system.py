7a9d55b1eec31542a6f5a73a1284242ab70b8a12
Jianhui Yang <jy7772792@gmail.com>
2026-04-22 23:30:58 +0800

    Try to build an CS2 Player Assessment System

---
+++ day3_assessment_system.py
@@ -0,0 +1,18 @@
+print("=" * 40)
+print("   CS2 PLAYER ASSESSMENT SYSTEM v1.0   ")
+print("=" * 40)
+
+player_name = input("player name: ")
+rating = float(input("rating: "))
+
+if rating >= 1.4:
+    print(f"{player_name} is one of the best CS2 players in the world!")
+elif rating >= 1.2:
+    print(f"{player_name} can be a Solid Starter.")
+elif rating >= 1.0:
+    print(f"{player_name} is just an average player.")
+else:
+    print(f"{player_name} might not be in good form right now.")
+
+print("   ASSESSMENT IS OVER!   ")
+print("=" * 40)
