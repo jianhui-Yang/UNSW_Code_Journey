6a6374da4708a282e62ac192bc6265ea2ef71fa2
Jianhui Yang <jy7772792@gmail.com>
2026-04-22 22:58:13 +0800

    day3_practice1

--- day1_chat.py
+++ day1_chat.py
@@ -1,7 +1,7 @@
-#接收用户的输入并存入变量
+#Receive user input and store it in a variable
 current_status = input("What's your visa status right now?")
 days_left = input("How many days until you fly to Sydney?")
 
 #use the f-string 
 print(f"\nDont worry, even if the status is {current_status}, you still have {days_left} days to get ready!")
-print ("Everything will on track!")
+print ("Everything will on!")
---
+++ day3_practice.py
@@ -0,0 +1,10 @@
+player1 = "Zywoo"
+player2 = "ropz"
+team_name = "vitality"
+
+rating = 1.20
+
+is_transform_confirmed = True
+
+print(f"\nIt is {is_transform_confirmed} that, {player1} and {player2} are now in {team_name}.")
+print(f"Their rating is around {rating} .")
